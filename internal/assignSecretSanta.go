df55b6ae4ee39d2833ac02df2019ba20673e3687
Sachi Nagada <sachinagada@gmail.com>
2018-02-06 22:39:39 -0500

    Changed assignment from hash to random generator

--- internal/assignSecretSanta.go
+++ internal/assignSecretSanta.go
@@ -2,54 +2,50 @@ package internal
 
 import (
 	"fmt"
-	"hash/fnv"
-	"math"
+	"math/rand"
 )
 
 var names = []string{"Sam", "Pam", "Michelle", "Sarah"}
-var spotsLeft = map[int]int{} //keeps track of all the indices that hanamee not been assigned yet to people
-
-//hash takes in a string and generates a hash number. This will be used to randomly select the people to be paired
-func hash(s string) uint32 {
-	h := fnv.New32()
-	h.Write([]byte(s))
-	return h.Sum32()
-}
 
 //AssignPeople tries to assign a secret santa to each person
 func AssignPeople() {
 	assigned := make([]string, len(names))
 
-	for i := range names {
-		spotsLeft[i] = i
-	}
-
-	//TODO: consider using a random number generator to assign secret santas
 	//TODO: compare the assigned slice with the names slice and if any of the names are the same, mix them up -- they are their own secret santas
 
 	for _, name := range names {
-		hashed := float64(hash(name))
-		index := int(math.Abs(hashed)) % len(names)
-		if assigned[index] == "" {
-			assigned[index] = name
-			delete(spotsLeft, index)
-		} else {
-			assigned = placeAssigned(assigned, name)
+		index := getRandomNumber(len(names))
+		for assigned[index] != "" { //keep randomly generating numbers until a free spot in assigned is found
+			fmt.Println("index", index)
+			index = getRandomNumber(len(names))
 		}
+		assigned[index] = name
+
+		fmt.Println(assigned)
+
 	}
-	fmt.Println("assigned string slice", assigned)
 }
 
-//TODO: play around with pointers so you don't hanamee to return the assigned back
-func placeAssigned(assigned []string, name string) []string {
-	var indexToRemove int
-	for k := range spotsLeft { // k will be selected randomly in the map
-		assigned[k] = name
-		indexToRemove = k
-		break
+func getRandomNumber(n int) int {
+
+	//TODO: look into seeding new sources for random generators:
+	/* s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)*/
+
+	i := rand.Intn(n * 77)
+	fmt.Println("random number", i)
+	return i % n
+}
+
+func finalAssign(assign []string) []string {
+	changeNames := []string{}
+
+	for i, v := range names {
+		if assign[i] == v {
+			changeNames = append(changeNames, v)
+		}
 	}
 
-	delete(spotsLeft, indexToRemove)
-
-	return assigned
+	//TODO: add the logic for changing the positions of the people who have themselves as their secret santa
+	return assign
 }
